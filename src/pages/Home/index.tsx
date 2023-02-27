167a2e59b1cfc693f364bead2edbbe9c39618b17
fernandoprestes <fernandoprestesc@gmail.com>
2023-02-27 18:06:53 -0300

    feat: add states forms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,47 @@
+import { useState } from 'react';
 import Header from '~/components/Header';
 import Tweet from '~/components/Tweet';
 import { NewTweetForm, NewTweetLabel, NewTweetButton, Separator } from './styles';
-
-const tweets = [
-  {
-    id: 1,
-    user: 'user',
-    userName: 'Fernando',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut officia porro earum repellendus, neque alias ipsa voluptates quam esse quia officiis illum rem magni ea nesciunt quis molestiae suscipit! Sapiente.',
-  },
-  {
-    id: 2,
-    user: 'user',
-    userName: 'Fernando',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut officia porro earum repellendus, neque alias ipsa voluptates quam esse quia officiis illum rem magni ea nesciunt quis molestiae suscipit! Sapiente.',
-  },
-];
+import type { FormEvent } from 'react';
 
 export const Home = function HomePages() {
+  const [tweets, setTweets] = useState([
+    {
+      id: 1,
+      user: 'user',
+      userName: 'Fernando',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut officia porro earum repellendus, neque alias ipsa voluptates quam esse quia officiis illum rem magni ea nesciunt quis molestiae suscipit! Sapiente.',
+    },
+    {
+      id: 2,
+      user: 'user',
+      userName: 'Fernando',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut officia porro earum repellendus, neque alias ipsa voluptates quam esse quia officiis illum rem magni ea nesciunt quis molestiae suscipit! Sapiente.',
+    },
+  ]);
+
+  const [newTweet, setNewTweet] = useState('');
+
+  function createNewTweet(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setTweets([
+      ...tweets,
+      {
+        id: Math.floor(Math.random() * 999),
+        user: 'user',
+        userName: 'Fernando',
+        content: newTweet,
+      },
+    ]);
+    setNewTweet('');
+  }
+
   return (
     <main>
       <Header title='Home' />
-      <NewTweetForm>
+      <NewTweetForm onSubmit={createNewTweet}>
         <NewTweetLabel htmlFor='tweet'>
           <img
             src='https://github.com/fernandoprestes.png'
@@ -32,6 +50,8 @@ export const Home = function HomePages() {
           <textarea
             id='tweet'
             placeholder="What's happening"
+            value={newTweet}
+            onChange={event => setNewTweet(event.target.value)}
           />
         </NewTweetLabel>
         <NewTweetButton type='submit'>Tweet</NewTweetButton>
--- src/pages/Status/index.tsx
+++ src/pages/Status/index.tsx
@@ -1,30 +1,45 @@
+import { useState } from 'react';
 import Header from '~/components/Header';
 import Tweet from '~/components/Tweet';
 import { AnswerTweetForm, AnswerTweetLabel, AnswerTweetButton, Separator } from './styles';
-
-const answers = [
-  {
-    id: 1,
-    content: 'lorem ipson',
-  },
-  {
-    id: 2,
-    content: 'lorem ipson',
-  },
-  {
-    id: 3,
-    content: 'lorem ipson',
-  },
-];
+import type { FormEvent } from 'react';
 
 export const Status = function StatusPages() {
+  const [answers, setAnswers] = useState([
+    {
+      id: 1,
+      content: 'lorem ipson',
+    },
+    {
+      id: 2,
+      content: 'lorem ipson',
+    },
+    {
+      id: 3,
+      content: 'lorem ipson',
+    },
+  ]);
+  const [newAnswers, setNewAnswers] = useState('');
+
+  function createNewAnswers(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setAnswers([
+      {
+        id: Math.floor(Math.random() * 999),
+        content: newAnswers,
+      },
+      ...answers,
+    ]);
+    setNewAnswers('');
+  }
+
   return (
     <main>
       <Header title='Tweet' />
       <Tweet content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure commodi ullam repudiandae alias ipsa doloribus eligendi. Earum adipisci magnam ex dignissimos itaque eos et expedita facilis voluptatum ipsam, suscipit rerum!' />
 
       <Separator />
-      <AnswerTweetForm>
+      <AnswerTweetForm onSubmit={createNewAnswers}>
         <AnswerTweetLabel htmlFor='tweet'>
           <img
             src='https://github.com/fernandoprestes.png'
@@ -33,6 +48,8 @@ export const Status = function StatusPages() {
           <textarea
             id='tweet'
             placeholder='Tweet your answer'
+            value={newAnswers}
+            onChange={event => setNewAnswers(event.target.value)}
           />
         </AnswerTweetLabel>
         <AnswerTweetButton type='submit'>Answer</AnswerTweetButton>
